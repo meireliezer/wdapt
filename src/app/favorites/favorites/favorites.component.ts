4aed05709986903323c787fbf5cd4785856722d4
meireliezer <meireliezer@hotmail.com>
2020-02-04 10:51:14 +0200

    toggle button

--- src/app/favorites/favorites/favorites.component.ts
+++ src/app/favorites/favorites/favorites.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-favorites',
@@ -8,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 export class FavoritesComponent implements OnInit {
 
 
-  constructor() { }
+  private _isGrid = false;
+  private activatedRouteSubscribtion;
+
+  public get isGrid() {
+    return this._isGrid;
+  }
+  
+  
+  constructor(private  activatedRoute: ActivatedRoute, private router: Router) {
+    
+  }
 
   ngOnInit() {
+    this._isGrid = (this.router.url.indexOf('grid') !== -1);
   }
+
+
+  public toggle(target:string) {
+    switch(target) {
+      case 'GRID':
+        this._isGrid = true;
+        this.router.navigate(['favorites/grid']);
+        break;
+      case 'TILES':
+        this._isGrid = false;
+        this.router.navigate(['favorites/tiles']);
+        break;
+    }
+
+  }
+
+
 
 }
