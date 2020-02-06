8b5869fc1cd6ca38b21ed2dfc39857389552c146
meireliezer <meireliezer@hotmail.com>
2020-02-06 18:33:49 +0200

    delete item complete

--- src/app/favorites/favorites-tiles/favorites-tiles.component.ts
+++ src/app/favorites/favorites-tiles/favorites-tiles.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FavoritesService } from 'src/app/common/favorites/favorites.service';
 import { Subscription } from 'rxjs';
 import { SearchService } from 'src/app/common/search/search.service';
+import { FavoriteItem } from 'src/app/model/favorite-item.interface';
+import { MyModalService } from 'src/app/core/my-modal/my-modal.service';
+import { DeleteComponent } from '../delete/delete/delete.component';
+import { ModalOk } from 'src/app/model/modal.interface';
 
 @Component({
   selector: 'app-favorites-tiles',
@@ -17,7 +21,10 @@ export class FavoritesTilesComponent implements OnInit, OnDestroy {
   private _searchSubscribe:Subscription;
 
   constructor(private favoritesService: FavoritesService,
-    private searchService: SearchService) {
+              private searchService: SearchService,
+              private modalService:MyModalService) {
+
+
       this._searchSubscribe = this.searchService.search$
       .subscribe(filterTxt => this.filterTxt = filterTxt);
    }
@@ -26,5 +33,19 @@ export class FavoritesTilesComponent implements OnInit, OnDestroy {
   }
   ngOnDestroy(): void {
     this._searchSubscribe.unsubscribe();
+  }
+
+
+  public delete(favorite: FavoriteItem){
+    this.modalService.openModal(DeleteComponent).subscribe( val => {
+      if(val.operation === ModalOk){
+        this.favoritesService.remove(favorite.id, favorite.websiteName, favorite.url);
+      }
+    });
+
+  }
+
+  public edit(favorite:FavoriteItem){
+
   }
 }
